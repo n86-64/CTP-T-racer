32bf89ce5bf5a4b1e850d65b8b9a6f137d791b34
Nathan <nathan.butt@gmail.com>
2018-10-16 14:59:00 +0100

    Implimented Vector3 core functions
    MAJOR CHANGES:
    * Implimented several vector3 core operations
    	- Addition
    	- Subtraction
    	- scalier multiplication
    	- magnitude/length
    	- normalisation
    
    * Added new lib folder for external dependancies.
    
    * Refined project settings.

--- T-Racer/src/core/main.cpp
+++ T-Racer/src/core/main.cpp
@@ -1,10 +1,24 @@
-
 #include <stdio.h>
+
+#include "helpers/Math_Vector.h"
 
 // The application entry point.
 int main(int argc, char* argv[]) 
 {
-	printf("Hello world, how are you.");
+	printf("Hello world, Today Im going to calculate a vector.");
+
+	T_racer_Vector3   vec1;
+	T_racer_Vector3   vec2(1.0f);
+	T_racer_Vector3   vec3(1.0f, 10.0f);
+	T_racer_Vector3   vec4(100.0f, 100.0f, 100.0f);
+	T_racer_Vector3   vec5;
+	vec5 += vec2;
+	vec3 = vec5 * 10.0f;
+	vec2 = vec3 * 2;
+
+
+	vec1 = vec4.normalise();
+
 	getchar();
 
 	return 0;
--- T-Racer/src/helpers/Math_Vector.h
+++ T-Racer/src/helpers/Math_Vector.h
@@ -1,7 +1,8 @@
 /** @file Math_Vector.h
 *  @brief A n-dimenstional column vector.
 *
-*  This defines a vector along with a set of operations.
+*  This defines a vector along with a set of operations for said vectors.
+*  Can be used to represent both colour and data.
 *
 *  @author Nathan Butt (n86-64)
 *  @bug No known bugs.
@@ -9,27 +10,57 @@
 
 #pragma once
 
+// A set of vector operations that can be called at any time. 
+namespace T_racer_Math 
+{
+	// TODO - Add inter-vector operations here. 
+
+}
+
 class T_racer_Vector3 
 {
 public:
-	T_racer_Vector3();
+	T_racer_Vector3() = default;
+	T_racer_Vector3(float x);
+	T_racer_Vector3(float x, float y);
+	T_racer_Vector3(float x, float y, float z);
+	T_racer_Vector3(const T_racer_Vector3&  vector);
 
-	float x();
-	float y();
-	float z();
-	float w();
+	// get/set components.
+	float x() { return values[0]; };
+	float y() { return values[1]; };
+	float z() { return values[2]; };
 
-	// Operators.
-	T_racer_Vector3 operator+(T_racer_Vector3& vec);
-	T_racer_Vector3& operator+=(T_racer_Vector3& vec);
-	T_racer_Vector3 operator-(T_racer_Vector3& vec);
-	T_racer_Vector3 operator-=(T_racer_Vector3& vec);
+	void x(float value) { values[0] = value; };
+	void y(float value) { values[1] = value; };
+	void z(float value) { values[2] = value; };
 
-	T_racer_Vector3 operator-=(T_racer_Vector3& vec);
+	// May be needed.
+	float*	getVectorAsArray();
+
+	// Linear operations
+	T_racer_Vector3 operator+(T_racer_Vector3 vec);
+	void			operator+=(T_racer_Vector3 vec);
+	T_racer_Vector3 operator-(T_racer_Vector3 vec);
+	void		    operator-=(T_racer_Vector3 vec);
+
+	// row based multiplication.
+	T_racer_Vector3  operator*(T_racer_Vector3 vec);
+	void			 operator*=(T_racer_Vector3 vec);
+	
+	T_racer_Vector3  operator*(float scalier);
+	T_racer_Vector3  operator*(int scalier);
+	void			 operator*=(float scalier);
+	void			 operator*=(int scalier);
+
+	// Other Vector Operations
+	T_racer_Vector3	normalise();
+
 private:
-	float values[3] = { 0.0f, 0.0f, 0.0f};
-	float magnitude; // Recalculated when a value is changed. Easy and simple.
+	float values[3] = { 0.0f, 0.0f, 0.0f };
 
-private: 	// Uitlity functions.
+
+private: 	
+	// Uitlity functions.
 	float calcualteMagnitude(); 
 };
