b492f4cc4bb30a3580c98ea2bfa33143a7172b9a
Nathan <nathan.butt@gmail.com>
2018-11-06 11:15:39 +0000

    Fixed issues with building Matrix4X4 project.

--- T-Racer/src/helpers/Math_Matrix.cpp
+++ T-Racer/src/helpers/Math_Matrix.cpp
@@ -1,10 +1,7 @@
 #include "Math_Matrix.h"
 
-T_racer_Math::Matrix4X4::Matrix4X4()
-{
-}
-
 T_racer_Math::Matrix4X4 T_racer_Math::Matrix4X4::operator*(T_racer_Math::Matrix4X4 mat)
 {
-	// TODO - Add a multiplication. 
+	// TODO - Add a multiplication function for a 4x4 martix.
+	return Matrix4X4();
 }
