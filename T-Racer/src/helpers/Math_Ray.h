0314a53706677126e33f2e161970a0cc4ddb9f27
Nathan <nathan.butt@gmail.com>
2018-11-29 15:12:39 +0000

    Added incoming ray direction and other fields.
    MAJOR CHANGES:
    * Added fields to store the incoming ray direction along with other data
    values.
    
    * Added code to store various values from the intersection.

--- T-Racer/src/helpers/Math_Ray.h
+++ T-Racer/src/helpers/Math_Ray.h
@@ -14,6 +14,7 @@
 
 namespace T_racer_Math 
 {
+	// Defines a ray located within world space.
 	class Ray 
 	{
 	public:
@@ -25,7 +26,7 @@ namespace T_racer_Math
 		Vector getInverseDirection();
 
 		void	setPosition(Vector pos) { position = pos; }
-		void	setDirection(Vector dir) { direction = dir; inverseDirection = direction.inverse(1); }
+		void	setDirection(Vector dir) { direction = dir; inverseDirection = direction.inverse(1); incomingRayDirection = -direction; }
 
 		float	getMagnitude() { return magnitude; }
 		void    setMagnitude(float newLength) { magnitude = newLength; }
@@ -34,6 +35,15 @@ namespace T_racer_Math
 		Vector  position;
 		Vector  direction;
 		Vector  inverseDirection; // TODO - Apply inverse direction to the ray.
+		Vector  incomingRayDirection;
 		float	magnitude = INFINITY; // Maybe need, not sure.
+
+		// Additional ray properties
+		Vector uv;
+		Vector normal;
+		Vector orthnormalBasis;
+		Vector hitPoint;
+
+		int	   BRDFMaterialID = 0;
 	};
 }
