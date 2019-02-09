a549e3c03bfcf43bf991826d90f2450f1ef88489
Nathan <nathan.butt@gmail.com>
2019-02-09 16:40:58 +0000

    Added template for glss material

--- T-Racer/src/core/MatMirrors.cpp
+++ T-Racer/src/core/MatMirrors.cpp
@@ -5,7 +5,7 @@
 
 #include "MatMirrors.h"
 
-T_racer_Math::Colour T_racer_Materials_Mirror::Evaluate(T_racer_Math::Ray * ray, T_racer_Path_Vertex & pathVertex)
+T_racer_Math::Colour T_racer_Materials_Mirror::Evaluate(T_racer_Math::Ray* ray, T_racer_Path_Vertex& pathVertex)
 {
 	T_racer_Math::Colour  lookupColour;
 	T_racer_Texture2D  outputTex(1, 1);
